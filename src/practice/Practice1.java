604423e69e16b963f2ed5dc88a60b4b8bc56f618
Benjamin <Benjamin@10.30.19.238>
2017-11-14 13:30:43 -0600

    First commit

---
+++ src/practice/Practice1.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        //Wrong way to declare a list
+//        ArrayList list = new ArrayList();
+        
+        //Correct way to declare a list
+        List<String> list = new ArrayList<>();
+        
+        //Only Objects can be stored in lists
+        
+        
+    }
+}
